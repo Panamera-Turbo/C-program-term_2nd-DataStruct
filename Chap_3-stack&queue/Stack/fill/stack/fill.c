5c98d250468d11f52c386e4c8f53c2315330acfc
Panamera-Turbo <1156742456@qq.com>
2020-07-30 21:41:22 +0800

    DataStruct

--- Chap_3-stack&queue/Stack/fill/stack/fill.c
+++ Chap_3-stack&queue/Stack/fill/stack/fill.c
@@ -3,6 +3,10 @@
 #include <stdbool.h>
 
 #include "stack.h"
+/**************************************
+ * 给出一个图形，其中#号代表边界，边界内部
+ * 的符号无关紧要，全部用 . 填充即可
+ * ***********************************/
 
 typedef char GRAPH[15][70];
 
@@ -27,23 +31,25 @@ GRAPH graph =
 
 const char boundary_color = '#';
 const char fill_color = '.';
-
+//---------------------------------------------------------------------------
 void draw(GRAPH g)
-{
+{//打印图形
     int i = 0;
     
     while (true)
     {
-        if (g[i][0] == '$') break;
+        if (g[i][0] == '$') 
+            break;
 
         printf("%s\n", g[i++]);
     }
 }
-
+//----------------------------------------------------------------------------
 void boundaryfill(GRAPH g, int seedx, int seedy)
-{
+{//填充图形
     Stack s, *S = &s;
-    Coord c = { seedx, seedy }, t;
+    Coord c = { seedx, seedy };
+    Coord t;
     Coord next[4] = { {0, -1}, {1, 0}, {0, 1}, {-1, 0}};
     int i;
 
