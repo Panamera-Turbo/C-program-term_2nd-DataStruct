7d80b191385e11ab4f49548bf7d860c7ddf31e03
Panamera-Turbo <1156742456@qq.com>
2020-08-12 18:43:43 +0800

    利用栈进行先序遍历

--- Chap_6-tree/05.10/1.c
+++ Chap_6-tree/05.10/1.c
@@ -17,22 +17,18 @@ typedef struct Node{
     struct Node* right;
 }BiTNode, *BiTree;
 // 遍历所使用栈的相关操作如下：
-
 #define Stack_Size 50
 typedef BiTNode* ElemType;
-
 typedef struct{
     ElemType elem[Stack_Size];
     int top;
 }Stack;
-
 void init_stack(Stack *S); // 初始化栈
 bool push(Stack* S, ElemType x); //x 入栈
 bool pop(Stack* S, ElemType *px); //出栈，元素保存到px所指的单元，函数返回true,栈为空时返回 false
 bool top(Stack* S, ElemType *px); //获取栈顶元素，将其保存到px所指的单元，函数返回true，栈满时返回 false
 bool is_empty(Stack* S);  // 栈为空时返回 true，否则返回 false
-
-
+//-----------------------------------------------------------------------------------------------------------------
 void pre_order(BiTree root)
 {
     Stack s, *S = &s;
