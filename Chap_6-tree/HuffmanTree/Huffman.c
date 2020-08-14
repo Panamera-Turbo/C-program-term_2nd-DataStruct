8cf57d01e5293d450a76e13f8955e3ea106bba71
Panamera-Turbo <1156742456@qq.com>
2020-08-14 23:07:34 +0800

    Huffman

--- Chap_6-tree/HuffmanTree/Huffman.c
+++ Chap_6-tree/HuffmanTree/Huffman.c
@@ -9,29 +9,32 @@ static void Select(HuffmanTree ht, int n, int *s1, int *s2)
 {
     int i, min;
 
-    min = -1;
+    min = -1;           //先设置最小值为-1，便于第一次的比较
     for (i = 1; i <= n; ++i)
-        if (ht[i].parent == 0 && (min == -1 || ht[i].weight < ht[min].weight))
+        if (ht[i].parent == 0 && (min == -1 || ht[i].weight < ht[min].weight))  
+            //注意这里的min==-1，必须写在前面，否则系统初次读取到ht[min]会越界，写在前面会执行后被略过后项。略危险的做法
             min = i;
+
     *s1 = min;
 
     min = -1;
     for (i = 1; i <= n; ++i)
         if (i != *s1 && ht[i].parent == 0 && (min == -1 || ht[i].weight < ht[min].weight))
             min = i;
+
     *s2 = min;
 }
 
 //----------------------------------------------------------------------------------------
 //创建Huffman树
 void CreateHuffmanTree(HuffmanTree ht, int *w, int n)
-{ //w:weight, 权值;
+{ //w:weight, 权值数组; n：已知的权重的数目
     int i, s1, s2;
     int m = 2 * n - 1;
 
-    for (i = 1; i <= m; ++i)
+    for (i = 1; i <= m; ++i)        //初始化权重和孩子、双亲
     {
-        ht[i].weight = i <= n ? w[i] : 0;
+        ht[i].weight = i <= n ? w[i] : 0;   //大于n的先留空，看下面的for就懂了
         ht[i].parent = ht[i].RChild = ht[i].LChild = 0;
     }
 
@@ -43,14 +46,6 @@ void CreateHuffmanTree(HuffmanTree ht, int *w, int n)
         ht[i].RChild = s2;
         ht[i].weight = ht[s1].weight + ht[s2].weight;
     }
-}
-//----------------------------------------------------------------------------------------
-//打印Huffman树
-void PrintHuffmanTree(HuffmanTree HT, int m)
-{
-    printf("节点 权重  双亲 左孩子 右孩子\n");
-    for (int i = 1; i <= m; ++i)
-        printf("%2d:(%4d, %4d, %4d, %4d)\n", i, HT[i].weight, HT[i].parent, HT[i].LChild, HT[i].RChild);
 }
 //----------------------------------------------------------------------------------------
 //创建Huffman编码:左子树为1, 右子树为0, 从根节点到该节点的数字连起来(二进制)就是对应的编码.
@@ -65,18 +60,26 @@ void CreateHuffmanCodeTable(HuffmanTree ht, HuffmanCodeTable hc, int n)
     cd[n] = '\0';            /*从右向左逐位存放编码，首先存放编码结束符*/
     for (i = 1; i <= n; ++i) /*求n个叶子结点对应的哈夫曼编码*/
     {
-        start = n;                                                     /*初始化编码起始指针*/
-        for (c = i, p = ht[i].parent; p != 0; c = p, p = ht[p].parent) /*从叶子到根结点求编码, c:child  p:parent*/
-            cd[--start] = ht[p].LChild == c ? '0' : '1';               //start往往到不了0,所以赋值给另外一个编码表cd里
+        start = n;                                                     //初始化编码起始指针
+
+        for (c = i, p = ht[i].parent; p != 0; c = p, p = ht[p].parent) //从叶子到根结点求编码, c:child  p:parent
+            cd[--start] = ht[p].LChild == c ? '0' : '1';               //start往往到不了0,所以赋值到另一编码表cd里
+
         strcpy(hc[i], &cd[start]);
     }
 }
-
 //----------------------------------------------------------------------------------------
-
 void PrintHuffmanCodeTable(HuffmanTree ht, HuffmanCode *hc, int n)
 {
     for (int i = 1; i <= n; ++i)
         printf("(%d) The node weighs %d was encoded as %s\n", i, ht[i].weight, hc[i]);
     printf("\n");
+}
+//----------------------------------------------------------------------------------------
+//打印Huffman树
+void PrintHuffmanTree(HuffmanTree HT, int m)
+{
+    printf("节点 权重  双亲 左孩子 右孩子\n");
+    for (int i = 1; i <= m; ++i)
+        printf("%2d:(%4d, %4d, %4d, %4d)\n", i, HT[i].weight, HT[i].parent, HT[i].LChild, HT[i].RChild);
 }
